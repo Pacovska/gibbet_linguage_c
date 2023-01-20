05a9aca78c4c4c009bd6c97d6ce2d41ef302ba71
PEDRO HENRIQUE PACOVSKA TOMAZ <105051234+Pacovska@users.noreply.github.com>
2023-01-20 20:36:15 -0300

    first commit

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+#define QUANTIDADE_CHUTE 26
+
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+int acertou();
+int enforcou();
+int jachutou(char letra);
+int chuteserrados();
